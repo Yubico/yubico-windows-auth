29fc71e72787e07dc313a156850f9043a42dadee
Klas Lindfors <klas@yubico.com>
2012-04-19 10:40:46 +0200

    add licensing information

--- yubiprovider/YubiKeyWrapExistingCredentialProvider/CWrappedCredentialEvents.h
+++ yubiprovider/YubiKeyWrapExistingCredentialProvider/CWrappedCredentialEvents.h
@@ -13,6 +13,38 @@
 // but LogonUI will not recognize the wrapped "this" pointer as a valid credential.
 // Our implementation translates from the wrapped "this" pointer to the wrapper "this".
 
+/* This file is based on sample code from the Microsoft Windows SDK
+    version 7.1. All changes are Copyright (c) 2012, Yubico AB.
+    All rights reserved.
+   
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following
+     disclaimer in the documentation and/or other materials provided
+     with the distribution.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+   ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+   TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+   THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+   SUCH DAMAGE.
+*/
+
+
 #pragma once
 
 #include <windows.h>
@@ -20,7 +52,6 @@
 #include <shlguid.h>
 #include "helpers.h"
 #include "dll.h"
-#include "resource.h"
 
 class CWrappedCredentialEvents : public ICredentialProviderCredentialEvents
 {
